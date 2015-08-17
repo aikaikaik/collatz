1f91cc35661daab57b8b3533de2f154517e3be17
arad keren <aradk1@gmail.com>
2015-08-17 11:22:35 +0300

    bug fix & "->'

--- calc.js
+++ calc.js
@@ -13,20 +13,20 @@ var calc = function calculate(n){
 	return out;
 };
 var calcToDom = function calculate_into_dom(n){
-	var output = document.getElementById("output");
-	var stats = document.getElementById("stats");
+	var output = document.getElementById('output');
+	var stats = document.getElementById('stats');
 	var result = calc(n);
 	output.innerHTML = result.join(' - ');
 	stats.innerHTML = result.length+' steps taken.';
 };
 calcToDom(42);
 
-var input = document.getElementById("input");
+var input = document.getElementById('input');
 input.onchange = function(){
 	var val = input.value;
 	var valInt = parseInt(val);
 	if(valInt.toString()===val && valInt>=1){
-		calcToDom(valint);
+		calcToDom(valInt);
 	}
 };
 
