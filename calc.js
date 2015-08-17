1e0fbc7dcfe4a8c06d820dd552219d067709b640
arad keren <aradk1@gmail.com>
2015-08-17 10:53:32 +0300

    split responsibility between functions

--- calc.js
+++ calc.js
@@ -1,37 +1,37 @@
-var output = document.getElementById("output");
-var stats = document.getElementById("stats");
-function calc(n){
+var calc = function calculate(n){
 
-	var steps = 0;
-	var out = "";
+	var out = [];
 	while(n!=1){
-		steps++;
-		out += n+" - ";
+		out.push(n);
 		if(n%2==0){
 			n = n/2;
 		}else{
 			n = 3*n+1;
 		}
 	}
-	out += "1";
-
-	output.innerHTML = out;
-	stats.innerHTML = steps+" steps taken.";
-
-}
-calc(42);
+	out.push(1);
+	return out;
+};
+var calcToDom = function calculate_into_dom(n){
+	var output = document.getElementById("output");
+	var stats = document.getElementById("stats");
+	var result = calc(n);
+	output.innerHTML = result.join(' - ');
+	stats.innerHTML = result.length+' steps taken.';
+};
+calcToDom(42);
 
 var input = document.getElementById("input");
 input.onchange = function(){
 	var val = input.value;
 	var valInt = parseInt(val);
 	if(valInt.toString()===val && valInt>=1){
-		calc(valInt);
+		calcToDom(valint);
 	}
 };
 
-function random(){
+var random = function calc_with_random(){
 	var n = Math.floor(Math.random()*1000);
 	input.value = n;
-	calc(n);
-}
+	calcToDom(n);
+};
