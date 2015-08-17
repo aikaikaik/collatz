0080c5fdc2f6c4027bd9c693d25081548dc04e7c
arad keren <aradk1@gmail.com>
2015-08-17 12:13:20 +0300

    max added

--- graph.js
+++ graph.js
@@ -1,8 +1,13 @@
+var getMax = function get_max_val_from_arr(arr){
+  return arr.reduce(function(pval,val,i,arr){
+    if(pval>val){return pval;}else{return val;}
+  },-Infinity);
+};
 var getData = function get_data_for_graphs(){
   var d = [];
   for(var i = 2; i < 800; i++){
     var result = calc(i);
-    d.push([i,result.length]);
+    d.push([i,result.length,getMax(result)]);
   }
   return d;
 };
@@ -10,14 +15,24 @@ var drawChart = function draw_google_chart(){
   var data = new google.visualization.DataTable();
   data.addColumn('number', 'begining number');
   data.addColumn('number', '# of steps');
+  data.addColumn('number', 'max');
   data.addRows(getData());
   var options = {
     chart: {
       title: 'collatz-eral'
-      ,subtitle: '# of steps for diffrent steps'
     }
     ,width: 450
     ,height: 500
+    ,series: {
+      0: {axis: 'steps'}
+      ,1: {axis: 'max'}
+    }
+    ,axes: {
+      y: {
+        steps: {label: '# of steps'}
+        ,max: {label: 'max num'}
+      }
+    }
   };
   var chart = new google.charts.Line(document.getElementById('linechart'));
   chart.draw(data, options);
